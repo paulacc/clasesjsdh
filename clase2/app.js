31b068e5ed5a6f3bf5d2e61ae03ff5de5bcdb8d7
paulacc <paulacasx@hotmail.com>
2018-10-26 21:42:45 -0300

    clasejs

---
+++ clase2/app.js
@@ -0,0 +1,16 @@
+var estudiantes = {
+  nombre: "Paula",
+  curso: "Fullstack",
+  dni: "99299292",
+  email : "paulacasx@gmail.com"
+
+  }
+
+   var myArray = [];
+   function fromObjectToArray (estudiantes){
+      for(const prop in obj){
+        myArray.push('prop');
+      }
+
+    console.log(myArray);
+  }
